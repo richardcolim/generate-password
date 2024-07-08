63b1c18efb53e66e74683be01cbb1931407b4a1d
richardcolim <130968909+richardcolim@users.noreply.github.com>
2024-07-08 12:44:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,22 @@
 //
 */
 const passwordBox = document.getElementById("password");
-const lenght = 6;
+const lenght = 12;
 const number = "0123456789";
+const upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+const lowerCase = "qwertyuiopasdfghjklzxcvbnm";
+const symbol = "@#$_&-+%/!?=*";
+
+const allChars = lowerCase + number + symbol + upperCase;
 function createPassword() {
     let password = "";
     password += number[Math.floor(Math.random() * number.length)];
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
 
     while(lenght > password.length) {
-        password += number[Math.floor(Math.random() * number.length)];
+        password += allChars[Math.floor(Math.random() * allChars.length)];
     }
     passwordBox.value = password;
 } 
